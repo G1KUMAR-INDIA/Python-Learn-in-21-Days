917cdd171ee407bec141ec3779ecb252da0f4263
R JEEVAN KUMAR <g1kumar215@gmail.com>
2025-03-09 10:11:00 +0530

    Python Update

---
+++ Day 08 Python Operators/Test.py
@@ -0,0 +1,113 @@
+# Arithmetic Operators
+# a=10
+# b=20
+# c=a+b # Addition
+# print("Addition of a and b",c)
+# c=a-b # Subtraction
+# print("Subtraction of a and b",c)
+# c=a*b # Multiplication
+# print("Multiplication of a and b",c)
+# c=a/b # Division
+# print("Division of a by b",c)
+# c=a%b # Modulus
+# print("Modulus of a by b",c)
+# c=a**b # Exponent
+# print("Exponent of a by b",c)
+# c=a//b # Floor Division
+# print("Floor Division of a by b",c)
+
+# Assignment Operators
+# a=10
+# b=20
+# c=a+b # Addition
+# print("Addition of a and b",c)
+# a+=b # a=a+b
+# print("Addition of a and b",a)
+# a-=b # a=a-b
+# print("Subtraction of a and b",a)
+# a*=b # a=a*b
+# print("Multiplication of a and b",a)
+# a/=b # a=a/b
+# print("Division of a by b",a)
+# a%=b # a=a%b
+# print("Modulus of a and b",a)
+# a**=b # a=a**b
+# print("Exponent of a to b",a)
+# a//=b # a=a//b
+# print("Floor Division of a by b",a)
+# x&=3 # x=x&3
+# print("Bitwise AND of x and 3",x)
+# x|=3 # x=x|3
+# print("Bitwise OR of x and 3",x)
+# x^=3 # x=x^3
+# print("Bitwise XOR of x and 3",x)
+# x>>=3 # x=x>>3
+# print("Right Shift of x by 3",x)
+# x<<=3 # x=x<<3
+# print("Left Shift of x by 3",x)
+
+# Comparison Operators or Relational Operators
+# a=10
+# b=20
+# c=a>b # Greater than
+# print("a is Greater than b:",c)
+# c=a<b # Less than
+# print("a is Less than  b:",c)
+# c=a>=b # Greater than or equal to
+# print("a is Greater than or equal to b:",c)
+# c=a<=b # Less than or equal to
+# print("a is Less than or equal to b:",c)
+# c=a==b # Equal to
+# print("a is Equal to b:",c)
+# c=a!=b # Not equal to
+# print("a is Not equal to b:",c)
+
+
+# Logical Operators is  comnbination of conditional statements
+# a=input("Enter your Logical value:")
+# b=input("Enter your Logical value:")
+# c=a and b # Logical AND
+# print("Logical AND a and b",c)
+# c=a or b # Logical OR
+# print("Logical OR a or b",c)
+# c=not a # Logical NOT
+# print("Logical NOT not a",c)
+
+# Bitwise Operators
+# a=10
+# b=20
+# c=a & b # Bitwise AND
+# print("Bitwise AND a and b",c)
+# c=a | b # Bitwise OR
+# print("Bitwise OR a or b",c)
+# c=a ^ b # Bitwise XOR
+# print("Bitwise XOR a ^ b",c)
+# c=a >> 2 # Bitwise Right Shift
+# print("Bitwise Right Shift a >> 2",c)
+# c=a << 2 # Bitwise Left Shift
+# print("Bitwise Left Shift a << 2",c)
+# c=~a # Bitwise NOT
+# print("Bitwise NOT ~a",c)
+
+# Membership Operators
+# a=3
+# b=[3,4,5]
+# c=a in b # Membership
+# print("Membership a in b",c)
+# c=a not in b # Not in
+# print("Not in a not in b",c)
+
+# Identity Operators
+# a=10
+# b=20
+# c=a is b # Identity
+# print("Identity a is b",c)
+# c=a is not b # Identity
+# print("Identity a is not b",c)
+
+# Ternary Operators
+# a=10
+# b=20
+# c=a if a>b else b # Ternary
+# print("Ternary a if a>b else b",c)
+
